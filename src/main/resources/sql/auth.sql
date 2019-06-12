4bffbd060dd722ce87a7b43f09e16da0b69daaf9
wen.liu <wen.liu@gemii.cc>
2019-06-12 10:36:24 +0800

    add auth.sql

---
+++ src/main/resources/sql/auth.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(13) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `username` varchar(33) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(33) DEFAULT NULL COMMENT '密码',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8
